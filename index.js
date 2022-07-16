81187914c45f0b4c3084f36f48740521b384904a
Three Worlds <harrymidas@yandex.ru>
2022-07-16 20:28:20 +0300

    feat: сверстать попап, реализовать открытие, закрытие, автозаполнение

---
+++ index.js
@@ -0,0 +1,55 @@
+let buttonEdit = document.querySelector('.profile__button-edit');
+let popup = document.querySelector('.popup');
+let buttonClose = document.querySelector('.popup__button-close');
+
+let formElement = document.querySelector('.popup__form');  //форма
+let nameInput = formElement.querySelector('.popup__input_type_name');  //строка имени в поп-апе
+let jobInput = formElement.querySelector('.popup__input_type_job'); //строка о работе в поп-апе
+
+let profileName = document.querySelector('.profile__name'); //строка имени на странице
+let profileDescription = document.querySelector('.profile__description'); //строка о работе на странице
+
+let buttonSave = formElement.querySelector('.popup__button-save');  //кнопка сохранить
+
+
+
+function setPopupInputValue() {                           // автозаполняет поля формы
+  nameInput.value = profileName.textContent.trim();
+  jobInput.value = profileDescription.textContent.trim();
+}
+
+function openPopap() {                      //открывает поп-ап автозаполняя поля
+  setPopupInputValue();
+  popup.classList.add('popup_opened');
+}
+
+function closePopap() {                         //закрывает поп-ап
+  popup.classList.remove('popup_opened');
+}
+
+function setProfileInformationValue() {             //устанавливает в профайл значения введенные в поля формы
+  profileName.textContent = nameInput.value;
+  profileDescription.textContent = jobInput.value;
+}
+
+function clickButtonSave() {          // устанавливает в профайл значения введенные в поля формы и закрывает поп-ап
+  setProfileInformationValue();
+  closePopap();
+}
+
+function formSubmitHandler(evt) {
+  clickButtonSave();
+  evt.preventDefault();
+}
+
+buttonSave.addEventListener('click', formSubmitHandler); // Закрытие поп-апа при нажатии "Сохранить"
+
+buttonEdit.addEventListener('click', openPopap);  // Открытие поп-апа
+
+buttonClose.addEventListener('click', closePopap); // Закрытие поп-апа
+
+formElement.addEventListener('submit', formSubmitHandler); //???????
+
+
+
+
